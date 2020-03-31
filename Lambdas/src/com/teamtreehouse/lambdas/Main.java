0f74931072f256aab31ee5e2e813e14335f65887
OshedheMunasinghe <oshedhe91@gmail.com>
2020-03-31 20:42:43 +0200

    Initial commit

---
+++ Lambdas/src/com/teamtreehouse/lambdas/Main.java
@@ -0,0 +1,53 @@
+package com.teamtreehouse.lambdas;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code here
+        // usingAnonymousInlineClass();
+        //l();
+
+
+    }
+
+   /*  //sorting alphabetic order in books
+    public static void usingAnonymousInlineClass() {
+        List<Book> books = Books.all();
+        Collections.sort(books, new Comparator<Book>() {
+            @Override
+            public int compare(Book b1, Book b2) {
+                return b1.getTitle().compareTo(b2.getTitle());
+            }
+        });
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }//end usingAnonymousInlineClass
+
+    */
+
+    /*
+     public static void usingLambdasInLongForm() {
+         List<Book> books = Books.all();
+         Collections.sort(books, (Book b1, Book b2 ->{ return b1.getTitle().compareTo(b2.getTitle()}));
+         for (Book book : books) {
+             System.out.println(book);
+         }
+     }//end usingLambdasInLongForm
+
+     */
+    /*
+    public static void l() {
+        List<Book> books = Books.all();
+        Collections.sort(books, (b1,b2) -> b1.getTitle().compareTo(b2.getTitle()));
+        books.forEach(book -> System.out.println(book));
+    }//end usingAnonymousInlineClass
+
+     */
+
+}//end class
